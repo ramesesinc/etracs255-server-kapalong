7ae689c0bb53218f5636b5cad525ed3f22875b30
lguddnkapalong <lguddnkapalong@gmail.com>
2020-06-03 13:36:52 +0800

    initial commit

---
+++ patches/v2.5.5_20190719.setup.ctc/v2.5.5_20190719.setup.ctc/res/scripts/mysql/patch_etracs.sql
@@ -0,0 +1,6 @@
+INSERT INTO `collectiontype` (`objid`, `state`, `name`, `title`, `formno`, `handler`, `allowbatch`, `allowonine`, `allowoffline`, `sortorder`, `allowpaymentorder`, `allowkiosk`, `allowcreditmemo`, `system`) 
+VALUES ('CTCC', 'ACTIVE', 'CTCC', 'CTC Corporate Collection', '0017', 'ctccorporate', '0', '1', '1', '0', '0', '0', '0', '0');
+
+INSERT INTO `collectiontype` (`objid`, `state`, `name`, `title`, `formno`, `handler`, `allowbatch`, `allowonine`, `allowoffline`, `sortorder`, `allowpaymentorder`, `allowkiosk`, `allowcreditmemo`, `system`) 
+VALUES ('CTCI', 'ACTIVE', 'CTCI', 'CTC Individual Collection', '0016', 'ctcindividual', '0', '1', '1', '0', '0', '0', '0', '0');
+
